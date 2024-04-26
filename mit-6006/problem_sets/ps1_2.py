8b3de43ad80a4105831c7b698ae4718f111a4f90
moemode <pfeffer.moritz@gmail.com>
2024-04-26 17:20:47 +0200

    Solve Problem 1-2

--- mit-6006/problem_sets/ps1_2.py
+++ mit-6006/problem_sets/ps1_2.py
@@ -1,4 +1,12 @@
 """Problem Set 1 Problem 2
+Given a data structure D that supports Sequence operations:
+• D.build(X) in O(n) time, and
+• D.insert at(i, x) and D.delete at(i), each in O(log n) time,
+where n is the number of items stored in D at the time of the operation, 
+describe algorithms to implement the following higher-level operations
+ in terms of the provided lower-level operations. 
+Each operation below should run in O(k log n) time. 
+Recall, delete at returns the deleted item.
 """
 
 from typing import Any
@@ -35,6 +43,33 @@ def reverse_iter(elements: list[Any], i: int, k: int):
         elements.insert(i + k - 1 - j, e)
 
 
+def move(elements: list[Any], fro: int, k: int, to: int):
+    """Move the k items in D starting at index i, in order,
+    to be in front of the item at index j.
+    Assume that expression i ≤ j< i + k is false.
+
+    Args:
+        elements (list): The list of elements to modify.
+        i (int): The starting index of the sublist to move.
+        k (int): The number of items to move.
+        j (int): The index of the item in front of which the sublist should be moved.
+
+    Returns:
+        None: This function modifies the input list in-place.
+
+    Raises:
+        IndexError: If the indices i, j, or i + k are out of range for the input list.
+    """
+    if fro < to:
+        for _ in range(k):
+            e = elements.pop(fro)
+            elements.insert(to, e)
+    elif to < fro:
+        for i in range(k):
+            e = elements.pop(fro + i)
+            elements.insert(to + i, e)
+
+
 if __name__ == "__main__":
     D1 = ["h", "a", "l", "o"]
     D2 = ["h", "a", "l", "o"]
@@ -42,3 +77,10 @@ if __name__ == "__main__":
     print(D1)
     reverse(D2, 1, 3)
     print(D2)
+
+    D3 = ["a", "b", "c", "d", "q", "q", "q"]
+    move(D3, 2, 2, 5)
+    print(D3)
+    D4 = ["q", "q", "q", "a", "b", "c", "d"]
+    move(D4, 4, 3, 2)
+    print(D4)
